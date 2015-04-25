f2496cf1f6943afc7f9ed4d98b7e5a42493375f6
Enrique Hernandez <enriqueohernandez@gmail.com>
2015-04-24 23:45:39 -0500

    DBHandler

---
+++ Jeopardy/src/java/DataBase/DBHandler.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DataBase;
+
+import User.Professor;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author enriqueohernandez
+ */
+public class DBHandler {
+
+    private static Connection connection;
+
+    public DBHandler() {
+        try {
+          
+            connection = DriverManager.getConnection("jdbc:mysql://104.131.19.102:3306/jeopardy", "root", "hinojosar00t");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public static boolean getLogin(Professor prof) {
+        boolean valid = false;
+        try {
+            Statement statement = connection.createStatement();
+            String email = prof.getEmail();
+            String password = prof.getPassword();
+            
+            ResultSet results = statement.executeQuery("CALL returnProfessorRow(" +email + ","+ password+ ")");
+            if (results.next()) {
+                prof.setFname(results.getString(1));
+                prof.setPassword(results.getString(2));
+                prof.setStatus(Integer.parseInt(results.getString(4)));
+                prof.setTries(Integer.parseInt(results.getString(5)));
+                valid = true;
+               /*
+               private String fname;
+    private String lname;
+    private String password;
+    private String email;
+    private int status;
+    private int tries;
+               */
+            }
+            statement.close();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(DBHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         return valid;   
+    }
+    /*
+    public static void storeMessage(Mensaje mensaje) {
+        try {
+            Statement statement = connection.createStatement();
+            String de = mensaje.getDe();
+            String para = mensaje.getPara();
+            String contenido = mensaje.getContenido();
+            String query = "insert into mensajes (de, para, contenido) values ('" + de + "','" + para + "','" + contenido + "')";
+            statement.executeUpdate(query);
+            statement.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBhandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public static ArrayList getMessages(String para) {
+        ArrayList list = new ArrayList();
+        try {            
+            Statement statement = connection.createStatement();
+            ResultSet results = statement.executeQuery("SELECT de, contenido, contenido FROM mensajes where para='"+para+"'");
+            while (results.next()) {
+                String de=results.getString(1);
+                String contenido=results.getString(2);
+                Mensaje mensaje = new Mensaje(de, para, contenido);
+                list.add(mensaje);
+            }
+            statement.close();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(DBhandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return list;
+    }
+    
+   
+        public static boolean addUser(Usuario usr) {
+             boolean valid = false;
+        try {
+            Statement statement = connection.createStatement();
+            String usuario = usr.getUser();
+            String password = usr.getPassword();
+            String query = "insert into usuarios (users, passwords) values ('" + usuario + "','" + password + "')";
+            statement.executeUpdate(query);
+            statement.close();
+            valid =true;
+        } catch (SQLException ex) {
+            Logger.getLogger(DBhandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+         return valid;
+
+    }
+    
+   */
+
+}
